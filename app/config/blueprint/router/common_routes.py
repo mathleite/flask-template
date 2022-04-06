17104705b89d2498dfdf666a9e9113d6bc7ce47a
Matheus Leite <matheus.leite.san@gmail.com>
2022-04-06 20:19:26 -0300

    create health-check route

--- app/config/blueprint/router/common_routes.py
+++ app/config/blueprint/router/common_routes.py
@@ -1,4 +1,5 @@
 from flask import Blueprint
+from datetime import datetime, timedelta, timezone
 
 common_routes = Blueprint('common_routes', __name__)
 
@@ -6,3 +7,12 @@ common_routes = Blueprint('common_routes', __name__)
 @common_routes.route('/')
 def hello_world():
     return "<p>Hello, World!</p>"
+
+
+@common_routes.route('/health-check')
+def health_check():
+    current_date = datetime.today().astimezone(timezone(timedelta(hours=-3)))
+    return {
+        'status': 200,
+        'datetime':  current_date.strftime('%Y-%m-%d %H:%M:%S')
+    }
